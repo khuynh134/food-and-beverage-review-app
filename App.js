eebea02c4f49d9a3b66e43daf8d545ccbd9aabfe
khuynh134 <113077001+khuynh134@users.noreply.github.com>
2023-09-12 14:35:24 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react'; 
+import {
+  StyleSheet, 
+  Button,
+  View,
+  Text 
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View        // Roughly where the searchbar will go 
+      style={{
+       width: 200,
+       height: 550,
+       padding: 40,
+      }}>
+        <Text>HomeScreen</Text>
+      </View>
+
+      
+
+
+  );
+};
+
+export default App; 
+ 
