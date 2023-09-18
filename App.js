098e7a8fb7a77807842e18558cd8e8e271229673
4srehome <4srehome@gmail.com>
2023-09-17 21:31:40 -0700

    Added the title & 'Add item' button; changed style

--- App.js
+++ App.js
@@ -16,31 +16,42 @@ const App = () => {
   return (
       
       <SafeAreaView style={styles.container}>
+        
+        <Text style={styles.title}>Food & Beverage Review App</Text>
+        
         <TouchableOpacity 
           onPress={ () => {
             console.log('button was pressed');
           }}
           style={styles.buttonContainer}
         >
-            <Text style={styles.buttonText2}>Restaurants</Text>
+            <Text style={styles.buttonText}>Restaurants</Text>
           
-
         </TouchableOpacity>
+
 
         <TouchableOpacity 
           onPress={ () => {
             console.log('button was pressed');
           }}
-          style={styles.brandsButtonContainer}
+          style={styles.buttonContainer}
         >
-          <Text style={styles.buttonText2}>Brands</Text>
+          <Text style={styles.buttonText}>Brands</Text>
       
+        </TouchableOpacity>
 
+
+        <TouchableOpacity 
+          onPress={ () => {
+            console.log('button was pressed');
+          }}
+          style={styles.buttonContainer2}
+        >
+          <Text style={styles.buttonText}>+ Add Item</Text>
+      
         </TouchableOpacity>
 
       </SafeAreaView>
-
-
   );
 };
 
@@ -51,65 +62,53 @@ const styles = StyleSheet.create({
     borderWidth:1, 
     padding:10,
   },
+  title: {
+    fontSize: 30,
+    color: '#44ccb4',
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderWidth: 4,
+    borderColor: '#545f71',
+    borderRadius: 6,
+    backgroundColor: '#edf2f1',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    position: 'absolute',
+    top: '3%',
+    right: '2%',
+    left: '25%',
+  },
   buttonContainer: {
-    height: 50,
-    marginHorizontal: 10,
+    height: 100,
+    marginHorizontal: '2%',
     backgroundColor: '#545f71',
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical:20,
   },
-  brandsButtonContainer: {
-    height: 50,
-    marginHorizontal: 10,
+  buttonContainer2: {
+    height: 100,
+    marginHorizontal: '2%',
     backgroundColor: '#545f71',
     justifyContent: 'center',
     alignItems: 'center',
-    marginVertical:20, 
+    marginVertical: 20,
+    position: 'absolute',
+    bottom: '3%',
+    width: '96%',
   },
   buttonText:{
     textTransform: 'uppercase',
     color: '#fff',
-    fontSize: 18,
-  },
-  buttonText2:{
-    textTransform: 'uppercase',
-    color: '#fff',
-    fontSize: 18,
-  },
-  buttonBrandContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    top: 370,
-    paddingVertical: 90,
-    paddingHorizontal: 60,
-    borderColor: 'black',
-  },
-  buttonRestContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    //position: 'absolute',
-    //top: 370,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    borderColor: 'black',
-  },
-  button: {
-    backgroundColor: 'green', 
-    borderColor: 'black',
-    borderRadius: 10,
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    fontSize: 22,
   },
   container: {
     flex: 1,
     //alignItems: 'center',
     justifyContent: 'center',
     //padding: 10,
+    backgroundColor: '#f0fbfa',
   },
 });
 
