d86c97f18f64c2752fafd910911e859e425ff5cc
4srehome <4srehome@gmail.com>
2023-09-20 00:20:15 -0700

    adjusted logo for screen; edited title background color

--- App.js
+++ App.js
@@ -22,7 +22,10 @@ const App = () => {
         
         <Text style={styles.title}>Food & Beverage Review App</Text>
 
-        <Image source={logo} style={{height: 100, width: 100}} />
+        //can't directly upload git repository into expo snack due to png file
+        //must copy-paste app.js, import logo file, then move it into assets folder to run
+        <Image source={logo} style={styles.logostyle} />
+        
         
         <TouchableOpacity 
           onPress={ () => {
@@ -70,19 +73,25 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     color: '#44ccb4',
-    marginTop: 10,
-    paddingVertical: 10,
+    paddingVertical: 5,
     paddingHorizontal: 5,
     borderWidth: 4,
     borderColor: '#545f71',
     borderRadius: 6,
-    backgroundColor: '#edf2f1',
+    backgroundColor: '#f3fafb',
     textAlign: 'center',
     fontWeight: 'bold',
     position: 'absolute',
-    top: '3%',
+    top: '4%',
     right: '2%',
-    left: '25%',
+    left: '30%',
+  },
+  logostyle: {
+    height: 100,
+    width: '25%',
+    position: 'absolute',
+    top: '4%',
+    left: '2%',
   },
   buttonContainer: {
     height: 100,
